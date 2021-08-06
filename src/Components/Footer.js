d61143e2b1a26a80bfe369b2c0371cff04e61459
your_username <your_email_address@example.com>
2021-08-06 01:07:02 +0100

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+           <p>Coccinella@designIn2021</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function footer() {
+    return (
+        <div className="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">Service</a>
+        <a href="#contact">Contact</a>
+        <a href="#contact">Expert</a>
+    <div className="topnav-right">
+        <a href="#search">Search</a>
+        <a href="#about">About</a>
+    </div>
+  </div>
+    )
+}
+
+export default footer
